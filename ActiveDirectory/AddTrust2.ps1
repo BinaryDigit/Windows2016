8e42a8b8c99e3e4bb935e207cc0144c62a0f6cdb
binarydigit <fguerricchio@binarydigit.it>
2018-01-24 16:21:25 +0100

    Add Trust2

--- ActiveDirectory/AddTrust2.ps1
+++ ActiveDirectory/AddTrust2.ps1
@@ -1,7 +1,9 @@
-New-ADDomainTrust -RemoteDomain "fabrikam.com" -RemoteAdmin "Administrator" -RemotePassword "P@ssw0rd" -TrustDirection "Outbound"   
+New-ADDomainTrust -RemoteDomain "contoso.com" -RemoteAdmin "Administrator" -RemotePassword "P@ssw0rd" -TrustDirection "Outbound"   
+New-ADDomainTrust -RemoteDomain "contoso.com" -RemoteAdmin "Administrator" -RemotePassword "P@ssw0rd" -TrustDirection "Inbound"   
   
-Remove-ADDomainTrust -RemoteDomain "fabrikam.com" -RemoteAdmin "Administrator" -RemotePassword "P@ssw0rd"  
+#Remove-ADDomainTrust -RemoteDomain "contoso.com" -RemoteAdmin "Administrator" -RemotePassword "P@ssw0rd"  
   
-New-ADForestTrust -RemoteForest "fabrikam.com" -RemoteAdmin "Administrator" -RemotePassword "P@ssw0rd" -TrustDirection "Outbound"  
+New-ADForestTrust -RemoteForest "contoso.com" -RemoteAdmin "Administrator" -RemotePassword "P@ssw0rd" -TrustDirection "Outbound"  
+New-ADForestTrust -RemoteForest "contoso.com" -RemoteAdmin "Administrator" -RemotePassword "P@ssw0rd" -TrustDirection "Inbound"  
    
-Remove-ADForestTrust -RemoteForest "fabrikam.com" -RemoteAdmin "Administrator" -RemotePassword "P@ssw0rd" 
+#Remove-ADForestTrust -RemoteForest "contoso.com" -RemoteAdmin "Administrator" -RemotePassword "P@ssw0rd" 
