566b1f1a81caebba7f471f745b5e4c1d3d499807
binarydigit <fguerricchio@binarydigit.it>
2018-01-24 15:55:38 +0100

    DNS Forwarder - updated

--- ActiveDirectory/AddForwarder.ps1
+++ ActiveDirectory/AddForwarder.ps1
@@ -1,1 +1,5 @@
-Add-DnsServerForwarder -IPAddress 192.168.137.126
+# Aggiunge due forwarders verso il dominio Contoso.com
+# Vanno aggiunti due forwareds poiche' il dominio Contoso.com ha due DC e relativi DNS
+
+Add-DnsServerForwarder -IPAddress [DC1.Contoso.com IPv4]
+Add-DnsServerForwarder -IPAddress [DC2.Contoso.com IPv4]
